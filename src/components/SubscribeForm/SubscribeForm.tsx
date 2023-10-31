aa3adb73a2e9701c158e2c5ad342c5d468028fb3
blkluv <info@blkluv.org>
2023-10-31 14:28:51 -0400

    quest: contact form

--- src/components/SubscribeForm/SubscribeForm.tsx
+++ src/components/SubscribeForm/SubscribeForm.tsx
@@ -7,117 +7,19 @@ const ContactForm = () => {
         <div className="max-w-md mx-auto my-10 bg-white p-5 rounded-md shadow-sm">
           <div className="text-center">
             <h1 className="my-3 text-3xl font-semibold text-gray-700 dark:text-gray-200">
-              Contact Us
+              Start HealN
             </h1>
             <p className="text-gray-400 dark:text-gray-400">
-              Fill up the form below to send us a message.
+              Start in the Waiting Area for free, subscribe to start your quest. 
             </p>
           </div>
           <div className="m-7">
-            <form
-              action="https://api.web3forms.com/submit"
-              method="POST"
-              id="form"
+            <button
+              onClick={() => window.location.href = 'https://discord.gg/EMrwEPwcQU'}
+              className="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none"
             >
-              <input
-                type="hidden"
-                name="access_key"
-                value="b5d8314d-e693-474c-9602-4eaecd6882bc"
-              />
-              <input
-                type="hidden"
-                name="subject"
-                value="New Submission from Web3Forms"
-              />
-              <input
-                type="hidden"
-                name="redirect"
-                value="https://web3forms.com/success"
-              />
-              <input
-                type="checkbox"
-                name="botcheck"
-                id=""
-                style={{ display: 'none' }}
-              />
-              <div className="mb-6">
-                <label
-                  htmlFor="name"
-                  className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
-                >
-                  Full Name
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name"
-                  placeholder="John Doe"
-                  required
-                  className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                />
-              </div>
-              <div className="mb-6">
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
-                >
-                  Email Address
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="info@healxyz.com"
-                  required
-                  className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                />
-              </div>
-              <div className="mb-6">
-                <label
-                  htmlFor="phone"
-                  className="text-sm text-gray-600 dark:text-gray-400"
-                >
-                  Phone Number
-                </label>
-                <input
-                  type="text"
-                  name="phone"
-                  id="phone"
-                  placeholder="+1 (555) 1234-567"
-                  required
-                  className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                />
-              </div>
-              <div className="mb-6">
-                <label
-                  htmlFor="message"
-                  className="block mb-2 text-sm text-gray-600 dark:text-gray-400"
-                >
-                  Your Message
-                </label>
-
-                <textarea
-                  rows="5"
-                  name="message"
-                  id="message"
-                  placeholder="Your Message"
-                  className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-                  required
-                ></textarea>
-              </div>
-              <div className="mb-6">
-                <button
-                  type="submit"
-                  className="w-full px-3 py-4 text-white bg-indigo-500 rounded-md focus:bg-indigo-600 focus:outline-none"
-                >
-                  Send Message
-                </button>
-              </div>
-              <p
-                className="text-base text-center text-gray-400"
-                id="result"
-              ></p>
-            </form>
+              HealXYZ Discord
+            </button>
           </div>
         </div>
       </div>
